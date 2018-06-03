107e22925aa7e58a0d2d46638d7eabaf4f2b0feb
Francis Bédard <francis.bedard.30@gmail.com>
2018-06-03 16:53:22 -0400

    First temp data layer added.

---
+++ TimeTracker.Data/Activity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker.Data
+{
+    public class Activity
+    {
+        public Activity(int _id, string _title, string _description, int _projectID, int _catID, int _userID)
+        {
+            ID = _id;
+            Title = _title;
+            Description = _description;
+            ProjectID = _projectID;
+            CategoryID = _catID;
+            userID = _userID;
+        }
+        public int ID { get; private set; }
+        public string Title { get; private set; }     
+
+        public string  Description { get; private set; }
+
+        public int ProjectID { get; private set; }
+
+        public int CategoryID { get; private set; }
+
+        public int userID { get; private set; }
+    }
+}
---
+++ TimeTrackerDesktop/BindableBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTrackerDesktop
+{
+    public class BindableBase : INotifyPropertyChanged
+    { 
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
